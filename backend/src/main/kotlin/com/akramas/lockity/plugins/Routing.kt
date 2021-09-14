8eb55eb0d7d2a8822552b0cc891a736fa7a4e339
Jokubas Akramas <j.akramas@gmail.com>
2021-09-14 14:37:49 +0300

    Test ep

--- backend/src/main/kotlin/com/akramas/lockity/plugins/Routing.kt
+++ backend/src/main/kotlin/com/akramas/lockity/plugins/Routing.kt
@@ -7,33 +7,35 @@ import io.ktor.application.*
 import io.ktor.response.*
 
 fun Application.configureRouting() {
-    
+
     install(Locations) {
     }
 
     routing {
-        get("/") {
-                call.respondText("Hello World!")
-            }
+        get("/test") {
+            call.respondText("Test!")
+        }
         // Static plugin. Try to access `/static/index.html`
         static("/static") {
             resources("static")
         }
         get<MyLocation> {
-                call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
-            }
-            // Register nested routes
-            get<Type.Edit> {
-                call.respondText("Inside $it")
-            }
-            get<Type.List> {
-                call.respondText("Inside $it")
-            }
+            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
+        }
+        // Register nested routes
+        get<Type.Edit> {
+            call.respondText("Inside $it")
+        }
+        get<Type.List> {
+            call.respondText("Inside $it")
+        }
     }
 }
+
 @Location("/location/{name}")
 class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
-@Location("/type/{name}") data class Type(val name: String) {
+@Location("/type/{name}")
+data class Type(val name: String) {
     @Location("/edit")
     data class Edit(val type: Type)
 
