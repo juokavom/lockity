c6dcb41376dafc521146db00fca6f5bf3f7d3cde
Jokubas Akramas <j.akramas@gmail.com>
2021-09-14 22:53:26 +0300

    Prod/dev switch case

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -3,11 +3,11 @@ type Server = {
 }
 
 const dev: Server = {
-    URL: 'https://localhost:500/'
+    URL: 'https://localhost:5000/'
 }
 
 const prod: Server = {
     URL: 'https://api.lockity.akramas.com/'
 }
 
-export const SERVER: Server = process.env.ENV === undefined && process.env.ENV !== "prod" ? dev : prod 
+export const SERVER: Server = process.env.NODE_ENV === "production" ? prod : dev
