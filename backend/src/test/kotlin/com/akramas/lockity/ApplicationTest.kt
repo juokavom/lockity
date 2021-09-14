e1d1fdeb52554dbeec30b5d83ae1b2054c25aec9
Jokubas Akramas <j.akramas@gmail.com>
2021-09-14 14:45:12 +0300

    Test fix

--- backend/src/test/kotlin/com/akramas/lockity/ApplicationTest.kt
+++ backend/src/test/kotlin/com/akramas/lockity/ApplicationTest.kt
@@ -9,9 +9,9 @@ class ApplicationTest {
     @Test
     fun testRoot() {
         withTestApplication({ configureRouting() }) {
-            handleRequest(HttpMethod.Get, "/").apply {
+            handleRequest(HttpMethod.Get, "/test").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("Hello World!", response.content)
+                assertEquals("Test!", response.content)
             }
         }
     }
