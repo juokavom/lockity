21ac017178ab85c8bd198085b469e202bacd3b75
Jokubas Akramas <j.akramas@gmail.com>
2021-11-23 15:00:54 +0200

    Remove uncompleted tests

--- backend/backend/src/test/kotlin/lockity/services/FileServiceTest.kt
+++ backend/backend/src/test/kotlin/lockity/services/FileServiceTest.kt
@@ -1,6 +1,5 @@
 package lockity.services
 
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream
 import database.schema.tables.records.FileRecord
 import database.schema.tables.records.SharedAccessRecord
 import database.schema.tables.records.UserRecord
@@ -22,6 +21,7 @@ import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.util.*
 import javax.naming.NoPermissionException
@@ -63,7 +63,7 @@ internal class FileServiceTest {
     fun `it must copy streams`() {
         val value = "test"
         val inputStream = value.byteInputStream()
-        val outputStream = ByteOutputStream()
+        val outputStream = ByteArrayOutputStream()
 
         fileService.copyTo(inputStream, outputStream).toString()
         assertEquals(
@@ -354,33 +354,33 @@ internal class FileServiceTest {
         )
     }
 
-    @Test
-    fun getUserReceivedFilesMetadata() {
-        fail("Not yet implemented")
-    }
-
-    @Test
-    fun getUserReceivedFilesMetadataCount() {
-        fail("Not yet implemented")
-    }
-
-    @Test
-    fun getDynamicLinkFileTitleLink() {
-        fail("Not yet implemented")
-    }
-
-    @Test
-    fun updateUserFile() {
-        fail("Not yet implemented")
-    }
-
-    @Test
-    fun modifyUserFileSharing() {
-        fail("Not yet implemented")
-    }
-
-    @Test
-    fun deleteFile() {
-        fail("Not yet implemented")
-    }
+//    @Test
+//    fun getUserReceivedFilesMetadata() {
+//        fail("Not yet implemented")
+//    }
+//
+//    @Test
+//    fun getUserReceivedFilesMetadataCount() {
+//        fail("Not yet implemented")
+//    }
+//
+//    @Test
+//    fun getDynamicLinkFileTitleLink() {
+//        fail("Not yet implemented")
+//    }
+//
+//    @Test
+//    fun updateUserFile() {
+//        fail("Not yet implemented")
+//    }
+//
+//    @Test
+//    fun modifyUserFileSharing() {
+//        fail("Not yet implemented")
+//    }
+//
+//    @Test
+//    fun deleteFile() {
+//        fail("Not yet implemented")
+//    }
 }
