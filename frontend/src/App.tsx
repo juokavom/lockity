67b26c75a81ba082c2cef3eb86a6715666d0203c
Jokubas Akramas <j.akramas@gmail.com>
2022-03-30 17:21:33 +0300

    Spinner icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,23 @@ import { BrowserRouter } from 'react-router-dom';
 import Main from './page/main/MainPage';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { TailSpin } from 'react-loader-spinner';
 
 function App() {
   return (
-      <BrowserRouter>
-        <div>    
-          <ToastContainer />
-          <Main />
-        </div>
-      </BrowserRouter>
+    <BrowserRouter>
+      <div>
+        <ToastContainer />
+        <TailSpin
+          height="100"
+          width="100"
+          color='grey'
+          ariaLabel='loading'
+        />
+        <Main />
+      </div>
+    </BrowserRouter>
   );
 }
 
